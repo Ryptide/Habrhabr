25ae7365c813c28234ff258e0b634e602ad63e5b
Kirill <kirill.oshkalo.1997@mail.ru>
2019-02-20 15:22:07 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -9,11 +9,16 @@ def init_db
 	@db.results_as_hash = true
 end
 
+#вызывается каждый раз при перезагрузке любой страницы
 before do
-		init_db
+	#инициализация БД
+	init_db
 end
 
+	#вызывается каждый раз при конфигурации приложения:
+	#когда изменился код программы и перезагрузилась страница
 configure do
+	#инициализация БД
 	init_db
 	@db.execute 'CREATE TABLE IF NOT EXISTS "Posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "create_date" DATE, "content" TEXT)'
 end
@@ -27,8 +32,7 @@ get '/new' do
 end
 
 post '/new' do
-	@db.execute
-
+	#получаем переменную из post запроса
 	content = params[:content]
 
 	erb "You typed #{content}"
